6ccc7d701fac96108e7675c89e683618f8239a7b
Evaldas Ciakas <evaldas757@gmail.com>
2015-09-03 13:03:59 +0300

    Log exceptions of fake-mcs code.

--- fake-mcs/Program.cs
+++ fake-mcs/Program.cs
@@ -9,80 +9,97 @@ namespace fake_mcs
   {
     static void Main(string[] args)
     {
-      var asseblyLocation = typeof(Program).Assembly.Location;
-      var sdkLocation = Path.GetFullPath(Path.Combine(
-        Path.GetDirectoryName(asseblyLocation), @"..\..\..\..\..\..\unityfull"
-      ));
+      var outText = new StringBuilder();
 
-      sdkLocation = Environment.GetEnvironmentVariable("UNITY_NEW_MONO_SDK") ?? sdkLocation;
+      try {
+        var asseblyLocation = typeof (Program).Assembly.Location;
+        var sdkLocation = Path.GetFullPath(Path.Combine(
+          Path.GetDirectoryName(asseblyLocation), @"..\..\..\..\..\..\unityfull"
+          ));
 
-      var mcsLocation = Environment.GetEnvironmentVariable("UNITY_NEW_MONO") 
-        ?? Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\Mono\lib\mono\4.5\mcs.exe");
+        sdkLocation =
+          Environment.GetEnvironmentVariable("UNITY_NEW_MONO_SDK") ??
+          sdkLocation;
 
-      var procStartInfo =
-        new System.Diagnostics.ProcessStartInfo(mcsLocation);
-      //        new System.Diagnostics.ProcessStartInfo(@"C:\Program Files (x86)\Mono\bin\mono.exe");
-      //        new System.Diagnostics.ProcessStartInfo(@"C:\Program Files (x86)\Mono\bin\mcs.bat");
+        var mcsLocation = Environment.GetEnvironmentVariable("UNITY_NEW_MONO")
+                          ??
+                          Environment.ExpandEnvironmentVariables(
+                            @"%ProgramFiles%\Mono\lib\mono\4.5\mcs.exe");
 
-      procStartInfo.Arguments = $@"""-sdk:{sdkLocation}"" --runtime:v2 " + args.Aggregate("", (s, s1) => s + " " + s1);
+        var procStartInfo =
+          new System.Diagnostics.ProcessStartInfo(mcsLocation);
+        //        new System.Diagnostics.ProcessStartInfo(@"C:\Program Files (x86)\Mono\bin\mono.exe");
+        //        new System.Diagnostics.ProcessStartInfo(@"C:\Program Files (x86)\Mono\bin\mcs.bat");
+
+        procStartInfo.Arguments = $@"""-sdk:{sdkLocation}"" --runtime:v2 " +
+                                  args.Aggregate("", (s, s1) => s + " " + s1);
 
 //      procStartInfo.Arguments = @"""C:\Program Files (x86)\Mono\lib\mono\4.5\mcs.exe"" " + procStartInfo.Arguments;
 
-      procStartInfo.UseShellExecute = false;
+        procStartInfo.UseShellExecute = false;
 //      procStartInfo.CreateNoWindow = true;
-      procStartInfo.RedirectStandardError = true;
-      procStartInfo.RedirectStandardOutput = true;
+        procStartInfo.RedirectStandardError = true;
+        procStartInfo.RedirectStandardOutput = true;
 
-      var proc = new System.Diagnostics.Process();
-      proc.StartInfo = procStartInfo;
-      proc.Start();
+        var proc = new System.Diagnostics.Process();
+        proc.StartInfo = procStartInfo;
+        proc.Start();
 
-      var output = new StringBuilder();
-      var error = new StringBuilder();
+        var output = new StringBuilder();
+        var error = new StringBuilder();
 
-      proc.OutputDataReceived += (sender, e) => {
-        Console.WriteLine(e.Data);
-        output.AppendLine(e.Data);
-      };
+        proc.OutputDataReceived += (sender, e) => {
+          Console.WriteLine(e.Data);
+          output.AppendLine(e.Data);
+        };
 
-      proc.ErrorDataReceived += (sender, e) => {
-        Console.Error.WriteLine(e.Data);
-        error.AppendLine(e.Data);
-      };
+        proc.ErrorDataReceived += (sender, e) => {
+          Console.Error.WriteLine(e.Data);
+          error.AppendLine(e.Data);
+        };
 
-      proc.BeginErrorReadLine();
-      proc.BeginOutputReadLine();
+        proc.BeginErrorReadLine();
+        proc.BeginOutputReadLine();
 
-      proc.WaitForExit();
+        proc.WaitForExit();
 
-      var outText = new StringBuilder();
-      outText.AppendLine(procStartInfo.Arguments);
+        outText.AppendLine(procStartInfo.Arguments);
 
-      var corlib_path = Path.GetDirectoryName(typeof(object).Assembly.Location);
-      string fx_path = corlib_path.Substring(0, corlib_path.LastIndexOf(Path.DirectorySeparatorChar));
-      outText.AppendLine(fx_path);
-      outText.AppendLine(asseblyLocation);
-      outText.AppendLine(sdkLocation);
+        var corlib_path =
+          Path.GetDirectoryName(typeof (object).Assembly.Location);
+        string fx_path = corlib_path.Substring(0,
+          corlib_path.LastIndexOf(Path.DirectorySeparatorChar));
+        outText.AppendLine(fx_path);
+        outText.AppendLine(asseblyLocation);
+        outText.AppendLine(sdkLocation);
 
 //      var dict = Environment.GetEnvironmentVariables();
 //      foreach (var k in dict.Keys) {
 //        outText.AppendLine(k + " " + dict[k]);
 //      }
-      outText.AppendLine(Environment.CurrentDirectory);
+        outText.AppendLine(Environment.CurrentDirectory);
 
-      outText.AppendLine();
-      outText.AppendLine("[Output start]");
-      outText.AppendLine();
-      outText.Append(output);
-      outText.AppendLine("[Output end]");
+        outText.AppendLine();
+        outText.AppendLine("[Output start]");
+        outText.AppendLine();
+        outText.Append(output);
+        outText.AppendLine("[Output end]");
 
-      outText.AppendLine();
-      outText.AppendLine("[Error start]");
-      outText.AppendLine();
-      outText.Append(error);
-      outText.AppendLine("[Error end]");
+        outText.AppendLine();
+        outText.AppendLine("[Error start]");
+        outText.AppendLine();
+        outText.Append(error);
+        outText.AppendLine("[Error end]");
+      }
+      catch (Exception e) {
+        outText.AppendLine();
+        outText.AppendLine("[Exception !!!]");
+        outText.Append(e);
+      }
 
-      System.IO.File.WriteAllText(Environment.ExpandEnvironmentVariables(@"%TEMP%\UnityMonoUpdatedCompilerOutput.txt"), outText.ToString());
+      System.IO.File.WriteAllText(
+          Environment.ExpandEnvironmentVariables(
+            @"%TEMP%\UnityMonoUpdatedCompilerOutput.txt"), outText.ToString());
     }
   }
 }
